8ad1e958d8020f9ab0d2d66c84d26c4344922ca7
HJ Kim <pknojlh27@naver.com>
2023-08-20 16:36:57 +0900

    feat: 12. 글 생성

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,7 +1,40 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Create() {
+  const router = useRouter();
   return (
-    <>
-      <>Create!!</>
-    </>
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const body = e.target.body.value;
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title, body }),
+        };
+        fetch(`http://localhost:9999/topics`, options)
+          .then(res => res.json())
+          .then(result => {
+            console.log(result);
+            const lastid = result.id;
+            router.push(`/read/${lastid}`);
+          });
+      }}
+    >
+      <p>
+        <input type="text" name="title" placeholder="title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body" />
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
   );
 }
