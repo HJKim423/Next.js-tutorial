75103ae3f281a5587cfeeeceaf84fdef0f4eea84
HJ Kim <pknojlh27@naver.com>
2023-08-20 16:24:47 +0900

    feat: 10.글 목록 가져오기

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,9 @@ export const metadata = {
 //a태그를 Link로 변경하면 SPA가 되어 속도가 향상된다.
 //페이지 전체를 변경하지 않고 변경되는 부분만 변경하며 한 번 방문했던 페이지는 소스를 다운로드 해놓은 데이터를 사용한다.
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const resp = await fetch("http://localhost:9999/topics");
+  const topics = await resp.json();
   return (
     <html>
       <body>
@@ -17,12 +19,13 @@ export default function RootLayout({ children }) {
           <Link href="/">WEB</Link>
         </h1>
         <ol>
-          <li>
-            <Link href="/read/1">html</Link>
-          </li>
-          <li>
-            <Link href="/read/2">css</Link>
-          </li>
+          {topics.map(topic => {
+            return (
+              <li key={topic.id}>
+                <Link href={`/read/${topic.id}`}>{topic.title}</Link>
+              </li>
+            );
+          })}
         </ol>
         {children}
         <ul>
